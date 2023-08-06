ebcba6ec085e15b0735c0c6a0530e54c4053d2d6
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-08-06 18:44:25 +0600

    code updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
+});
